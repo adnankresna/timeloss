a5462e8de1235242141f71cb8a85cd13e23bf77d
adnankresna <kresnaa23@gmail.com>
2025-03-13 00:56:47 +0700

    Speed up theme transition to 0.2s for a snappier feel

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -21,10 +21,10 @@ export function ThemeToggle() {
       size="icon"
       onClick={toggleTheme}
       aria-label="Toggle theme"
-      className="relative overflow-hidden transition-all duration-500 ease-in-out"
+      className="relative overflow-hidden transition-all duration-200 ease-in-out"
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-500 dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-500 dark:rotate-0 dark:scale-100" />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-200 dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-200 dark:rotate-0 dark:scale-100" />
     </Button>
   );
 } 
